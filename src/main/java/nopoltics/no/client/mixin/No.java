c6def573e3c8a6e7d1383054cb482265848aa217
Deximus-Maximus <kev12east@gmail.com>
2020-09-20 04:34:02 -0400

    Exclude advancements

--- src/main/java/nopoltics/no/client/mixin/No.java
+++ src/main/java/nopoltics/no/client/mixin/No.java
@@ -2,6 +2,7 @@ package nopoltics.no.client.mixin;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.advancement.AdvancementsScreen;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,7 +14,8 @@ public class No {
     @Inject(at = @At("HEAD"), method = "openScreen(Lnet/minecraft/client/gui/screen/Screen;)V", cancellable = true)
     private void no(@Nullable Screen screen, CallbackInfo ci) {
         if (screen == null) return;
-        if (screen.getClass().getPackage().getName().contains("vazkii") && !screen.getClass().getPackage().getName().contains("book")) {
+        if (screen.getClass().getPackage().getName().contains("vazkii") &&
+                !(screen.getClass().getPackage().getName().contains("book") || screen instanceof AdvancementsScreen)) {
             ci.cancel();
         }
     }
