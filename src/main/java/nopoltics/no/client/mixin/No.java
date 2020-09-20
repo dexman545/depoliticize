c241d27a3b653cb85ad376329a9d324068f5f45d
Deximus-Maximus <kev12east@gmail.com>
2020-09-20 04:24:53 -0400

    Remove fapi requirement Remove debug

--- src/main/java/nopoltics/no/client/mixin/No.java
+++ src/main/java/nopoltics/no/client/mixin/No.java
@@ -13,7 +13,6 @@ public class No {
     @Inject(at = @At("HEAD"), method = "openScreen(Lnet/minecraft/client/gui/screen/Screen;)V", cancellable = true)
     private void no(@Nullable Screen screen, CallbackInfo ci) {
         if (screen == null) return;
-        System.out.println(screen.getClass().getPackage().getName());
         if (screen.getClass().getPackage().getName().contains("vazkii") && !screen.getClass().getPackage().getName().contains("book")) {
             ci.cancel();
         }
